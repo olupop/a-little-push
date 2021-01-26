a4f4c10f02354704f1acca186e77a15f32e6bd57
Malik Tekin <malik.tekin@mailfence.com>
2021-01-26 10:29:42 -0500

    Print the entered names in a single string, separated by commas.

---
+++ names-separated-by-comma.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int initiated = 0;
+
+char *InitiateString(char *str, int len)
+{
+		str = (char *)realloc(str, len + 1);
+		return str;
+}
+
+char *AddName(char *str, char *name)
+{
+	int name_len = strlen(name);
+	int str_len;
+	int temp;
+	
+	if (!(initiated))
+	{
+		str = InitiateString(str, name_len);
+		strcpy(str, name);
+		initiated = 1;
+	}
+	else
+	{
+		str_len = strlen(str);
+		temp = (char *)realloc(str, str_len + name_len + 3); // ',' + ' ' + '\0' -> 3 byte
+		if (str == NULL)
+		{
+			printf("Cannot allocate memory!!\n");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			str = temp;
+			strcat(str, ", ");
+			strcat(str, name);
+		}
+	}
+	
+	return str;	
+}
+
+int main()
+{
+	char name[64];
+	char *names = NULL;
+	
+	while(1)
+	{
+		printf("Please enter a name or \"exit\": ");
+		gets(name);
+		if(!(strcmp(name, "exit")))
+			break;
+		else
+			names = AddName(names, name);
+	}
+	puts(names);
+	
+	return 0;
+}
